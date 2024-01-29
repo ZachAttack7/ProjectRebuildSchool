a33025212065ebf0af05100d398f222950e380a0
ZachAttack7 <153025683+ZachAttack7@users.noreply.github.com>
2024-01-29 11:51:36 -0500

    Create thing.js

---
+++ thing.js
@@ -0,0 +1,1 @@
+javascript:(function () {var a = document.createElement('script');a.src = 'https://raw.githubusercontent.com/ZachAttack7/ProjectRebuildSchool/main/ingot.js';document.body.appendChild(a);}())
